a2df58a5f03fcd011214772e49db04599977c1ad
Mark Quinn <mobious_99@yahoo.com>
2024-11-29 09:54:57 -0500

    fixed docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "template"
 copyright = "2024, Mark Quinn"
 author = "Mark Quinn"
-release = "0.0.0"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
